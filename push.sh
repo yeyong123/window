10f374f1c9c9b65577b76e93a94ecb954d7739a4
yeyong <yeyong14@gmail.com>
2018-03-05 15:46:47 +0800

    部署

--- push.sh
+++ push.sh
@@ -3,4 +3,4 @@ git commit -m "$1$2"
 git push origin master
 git push dev master
 
-ssh 5mutian_one 'cd /srv/www/flask_windo && git pull origin master --no-edit'
+ssh 5mutian_one 'cd /srv/www/flask_window && git pull origin master --no-edit'
